87e578064a6c0a8b792b285b2930e2ec25fd2069
emrecoban <fbdarktole@gmail.com>
2023-04-06 21:06:31 +0300

    Updated readme

--- README.md
+++ README.md
@@ -2,5 +2,35 @@
 
 With the help of JavaScript, Firebase Realtime Database, and HTML Geolocation API, this application allows users to track their current location in real-time and displays it on a map. It provides a seamless user experience and helps individuals keep track of their location without any hassle.
 
+## Features
+- Show realtime locations
+- Browser-based
+- Marker info
+
+## Usage
+1. Go to [locationtracker.vercel.app](https://locationtracker.vercel.app) in any device.
+2. Enter a nickname, and then click on the "Add my location to the map".
+3. See all the locations includes you in database.
+
+## Screenshots
+![homepage](github_assets/ss1.png)
+
+## Built with
+- JavaScript
+- Firebase Realtime Database
+- HTML Geolocation API
+- Google Maps API
+
+## Support
+- Sponsor to me on GitHub.
+- Give a star to this repo.
+- Follow me on Twitter [@emreshepherd](https://twitter.com/emreshepherd), or GitHub [@emrecoban](https://github.com/emrecoban).
+- Buy me a coffee, or book: https://www.buymeacoffee.com/emrecoban
+
+## References
+- [HTML Geolocation API](https://www.w3schools.com/html/html5_geolocation.asp)
+- [Google Maps JavaScript API](https://developers.google.com/maps/documentation/javascript)
+- [Firebase Realtime Database](https://firebase.google.com/docs/database/web/start?hl=en#web-version-9)
+
 ## License
 This project is available as open source under the terms of the [MIT License](https://github.com/emrecoban/realtimeLocationTracker/blob/main/LICENSE).
--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const nickName = document.getElementById("nickname");
 const addBtn = document.getElementById("addBtn");
-const locationsEl = document.getElementById("locations");
 
 addBtn.addEventListener("click", () => {
   if (nickName.value == "") {
@@ -21,6 +20,7 @@ addBtn.addEventListener("click", () => {
   } else {
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition(setLocation);
+      
     } else {
       alert("Geolocation is not supported by this browser.");
     }
@@ -40,7 +40,7 @@ function setLocation(position) {
   getLocations();
 }
 
-let markers = []
+let markers = [];
 
 let map;
 async function initMap() {
@@ -57,42 +57,40 @@ function getLocations() {
   onValue(ref(database, "locations"), (snapShot) => {
     const data = snapShot.val();
     const infoWindow = new google.maps.InfoWindow(); // Marker Info
-    console.log("Gelen veri => ", Object.values(data));
     removeMarkers();
-    markers = []
-    Object.values(data).map((locData)=>{
-        const newMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(locData.latitude, locData.longitude),
-            map: map,
-            icon: "img/navigation.png",
-            title: locData.label,
-            optimized: false,
-        });
-        newMarker.addListener("click", ()=>{
-            infoWindow.close();
-            infoWindow.setContent(newMarker.getTitle());
-            infoWindow.open(newMarker.getMap(), newMarker);
-        })
-        markers.push(newMarker);
-    })
+    markers = [];
+    Object.values(data).map((locData) => {
+      const newMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(locData.latitude, locData.longitude),
+        map: map,
+        icon: "img/navigation.png",
+        title: locData.label,
+        optimized: false,
+      });
+      newMarker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(newMarker.getTitle());
+        infoWindow.open(newMarker.getMap(), newMarker);
+      });
+      markers.push(newMarker);
+    });
     showMarkers();
   });
 }
 
-const setMapOnAll = (map)=>{
-    for (let i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-    }
-}
+const setMapOnAll = (map) => {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+};
 
-const showMarkers = ()=>{
-    setMapOnAll(map);
-}
+const showMarkers = () => {
+  setMapOnAll(map);
+};
 
-const removeMarkers = ()=>{
-    setMapOnAll(null);
-    markers = [];
-}
+const removeMarkers = () => {
+  setMapOnAll(null);
+  markers = [];
+};
 
 console.log("Init database => ", database);
-// Google Maps API: AIzaSyAGCkfuPFMFAj-KloukOh3EyWJ7KceCQnY
